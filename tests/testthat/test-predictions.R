71edc5919ce88e3d5bd5cc185213ca46c83d002b
ecodata1 <tim.foster@sepa.org.uk>
2021-08-19 16:24:38 +0100

    tests: fix failing tests

--- tests/testthat/test-predictions.R
+++ tests/testthat/test-predictions.R
@@ -4,19 +4,18 @@ test_that("metrics works", {
     select(sample_number, indices) %>%
     unnest(indices) %>%
     group_by(quality_elements) %>%
-    slice_sample(n = 4)
+    dplyr::slice_sample(n = 4)
 })
 
 test_that("metrics works", {
   classification(demo_data) %>%
     group_by(quality_elements) %>%
-    slice_sample(n = 4) %>%
+    dplyr::slice_sample(n = 4) %>%
     select(sample_number, classification)
 })
 
 
 test_that("predictions works", {
-
-  test <- suppressWarnings(prediction(demo_data))
+  test <- suppressWarnings(prediction(demo_data[1:1000, ]))
   expect_equal(length(test), 24)
 })
