948da2558d030f3c6150be0168027cdccd296b58
ecodata1 <tim.foster@sepa.org.uk>
2022-02-01 16:40:05 +0000

    feat: placeholder function for integration of non-wfd assessment

---
+++ R/rat_prediction.R
@@ -0,0 +1,13 @@
+#' @importFrom rlang .data
+#' @importFrom tibble tibble
+#' @importFrom dplyr group_by inner_join mutate select arrange filter summarise ungroup bind_rows
+#' @importFrom tidyr unnest nest pivot_wider
+#' @importFrom magrittr `%>%`
+#' @importFrom purrr map
+
+rat_prediction <- function(data) {
+ data <- whpts::whpt_predict(data)
+ data <- select(data, index, predicted_response)
+
+ return(data)
+}
