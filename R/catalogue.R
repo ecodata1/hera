bc92c21eef536bb824c5b6a47fdd23ac89bff37e
Foster <Tim.Foster@SEPA.org.uk>
2022-11-24 13:52:42 +0000

    docs: experimental vocab glossary, update convert docs.

--- R/catalogue.R
+++ R/catalogue.R
@@ -2,9 +2,10 @@
 #'
 #' A data frame containing descriptions, data and functions used by assessment
 #' methods. This catalogue is automatically extended by knitting new assessment
-#' vignettes and rebuilding the package.
+#' vignettes and rebuilding the package. See Development Guide on package
+#' website.
 #'
-#' @format A data frame with 4 variables:
+#' @format A data frame with 3 variables:
 #' \describe{
 #'   \item{assessment}{Name of assessment}
 #'   \item{data}{List data frame describing the input and output data of the
@@ -12,5 +13,5 @@
 #'   \item{assessment_function}{Assessment function that generates calculated
 #'   output values}
 #' }
-#' @source Agency sampling data
+#' @source See each assessment for detailed references.
 "catalogue"
