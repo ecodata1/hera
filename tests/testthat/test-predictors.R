3963b35e7fa6d85b7ecd7cef301cd9d374a6e87e
Foster <Tim.Foster@SEPA.org.uk>
2025-03-31 17:06:39 +0100

    tests: update ngr testing

--- tests/testthat/test-predictors.R
+++ tests/testthat/test-predictors.R
@@ -7,11 +7,22 @@ test_that("NGR formatting", {
 
   # Check for spaces in grid_reference (need to split grid reference for RICT)
   # See rict.Rmd in hera package
+
+
+    predictors$date <-  as.Date(predictors$date)
+    # use latest 'version' for each location_id
+    predictors <- dplyr::arrange(predictors, desc(date))
+    predictors <- predictors[!duplicated(predictors$location_id), ]
+
+
    errors <- predictors[!grepl(" ", predictors$grid_reference), ]
    errors2 <- predictors[!grepl(" ", predictors$original_grid_reference), ]
 
-   errors <- bind_rows(errors, errors2)
-   errors <- distinct(errors)
+   errors <- dplyr::bind_rows(errors, errors2)
+   errors <- dplyr::distinct(errors)
+   # if ngr blank - exclude (i.e. diatom only chemsitry sife predictor)
+   errors <- dplyr::filter(errors, grid_reference != "" |
+                      original_grid_reference != "")
 
    testthat::expect_equal(nrow(errors), expected = 0)
 })
