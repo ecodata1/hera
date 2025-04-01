61a6fff2075941db2525969a29a930775d2b7394
Foster <Tim.Foster@SEPA.org.uk>
2025-04-01 17:13:42 +0100

    tests: predictors match against sampling point table.

--- tests/testthat/test-predictors.R
+++ tests/testthat/test-predictors.R
@@ -20,9 +20,48 @@ test_that("NGR formatting", {
 
    errors <- dplyr::bind_rows(errors, errors2)
    errors <- dplyr::distinct(errors)
-   # if ngr blank - exclude (i.e. diatom only chemsitry sife predictor)
+   # if ngr blank - exclude (i.e. diatom only chemistry site predictor)
    errors <- dplyr::filter(errors, grid_reference != "" |
                       original_grid_reference != "")
 
    testthat::expect_equal(nrow(errors), expected = 0)
+})
+
+
+test_that("Check NGR are correct", {
+  testthat::skip(message = "needs local network connection")
+
+  predictors <- utils::read.csv(system.file("extdat",
+                                            "predictors.csv",
+                                            package = "hera"
+  ), check.names = FALSE)
+
+  predictors$date <-  as.Date(predictors$date)
+  # use latest 'version' for each location_id
+  predictors <- dplyr::arrange(predictors, desc(date))
+  predictors <- predictors[!duplicated(predictors$location_id), ]
+
+
+
+ sampling_points <- hera::get_data(
+   location_id = predictors$location_id,
+   dataset = "sampling_points")
+
+ sampling_points <- dplyr::select(sampling_points, sampling_point, ngr)
+ predictors <- dplyr::left_join(
+   predictors,
+   sampling_points,
+   by = dplyr::join_by(location_id == sampling_point))
+
+ testthat::expect_equal(predictors$original_grid_reference, predictors$ngr)
+
+ # Due to some location not being in the temperature grid for RICT their ngr
+ # is different from 'original_grid_reference'.
+
+ mismatch_ngr <- predictors[predictors$grid_reference != predictors$ngr,]
+
+ # These ngrs are valid mismatches because the RICT temperature grid not
+ # covering all land area
+ testthat::expect_equal(mismatch_ngr$location_id, 134730)
+
 })
